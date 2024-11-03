36ba233fb01b76a43904c898b21a3d538d9dbfbe
Joaofelismino <felismino18@outlook.com>
2024-11-03 04:40:50 -0300

    adicionando comentario

--- src/index.js
+++ src/index.js
@@ -1,59 +1,58 @@
-//variáveis gobais
-const restaurante = {
-  mesa : {
-    numeroMesa : "2",
-    quantMaxPessoas : 8,
-    pedido : ""
-  },
-  pedidos : {
-    pedido1 :{
-      idPedido : "000001",
-      itens : {
-        item1 : {
-          numero : "54",
-          produto : "Pizza G de calabresa ",
-          descricao : "Pizza de calabresa tamanho Grande de 8 pedaços ",
-          bordasRecheadas : "sim",
-          valor : 55,
-          quantidade : 1
-        },
-        item2 : {
-          numero : "34",
-          produto : "Pizza G de Frango com requeijão ",
-          descricao : "Pizza de requeijão tamanho Grande de 8 pedaços ",
-          bordasRecheadas : "não",
-          valor : 60,
-          quantidade : 1
-        },
-        item3 : {
-          numero : "108",
-          produto : "coca cola 2lts",
-          descricao : "Refrigerante coca cola de 2 Litros",
-          valor : 11,
-          quantidade : 2
-        }
-      }, 
-    },
-    pedido2 : {
-      idPedido : "000002",
-      itens : {
-        item1 : {
-          numero : "65",
-          produto : "Esfiha de carne ",
-          descricao : "Esfiha  de carne assada ",
-          bordasRecheadas : "não",
-          valor : 50,
-          quantidade : 2
-        }
-      }  
-    }
-  },
-  usuario : {
-    matricula : "001",
-    tipoDeUsuário : "Garçon"
-  }
+// //variáveis gobais
+// const restaurante = {
+//   mesa : {
+//     numeroMesa : "2",
+//     quantMaxPessoas : 8,
+//     pedido : ""
+//   },
+//   pedidos : {
+//     pedido1 :{
+//       idPedido : "000001",
+//       itens : {
+//         item1 : {
+//           numero : "54",
+//           produto : "Pizza G de calabresa ",
+//           descricao : "Pizza de calabresa tamanho Grande de 8 pedaços ",
+//           bordasRecheadas : "sim",
+//           valor : 55,
+//           quantidade : 1
+//         },
+//         item2 : {
+//           numero : "34",
+//           produto : "Pizza G de Frango com requeijão ",
+//           descricao : "Pizza de requeijão tamanho Grande de 8 pedaços ",
+//           bordasRecheadas : "não",
+//           valor : 60,
+//           quantidade : 1
+//         },
+//         item3 : {
+//           numero : "108",
+//           produto : "coca cola 2lts",
+//           descricao : "Refrigerante coca cola de 2 Litros",
+//           valor : 11,
+//           quantidade : 2
+//         }
+//       }, 
+//     },
+//     pedido2 : {
+//       idPedido : "000002",
+//       itens : {
+//         item1 : {
+//           numero : "65",
+//           produto : "Esfiha de carne ",
+//           descricao : "Esfiha  de carne assada ",
+//           bordasRecheadas : "não",
+//           valor : 50,
+//           quantidade : 2
+//         }
+//       }  
+//     }
+//   },
+//   usuario : {
+//     matricula : "001",
+//     tipoDeUsuário : "Garçon"
+//   };
 
-};
 
 function abrirModal(event) {
   // Não deixa recarregar a página ao abrir o modal
